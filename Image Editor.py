9570d8445c4a8aa809f8cc82faaafb030d23740a
이진영 <jakerhee321@gmail.com>
2023-01-26 12:17:10 +0900

    Buttons and Boxes

--- Image Editor.py
+++ Image Editor.py
@@ -1,9 +1,12 @@
 import PySimpleGUI as sg
 
-#control_col = sg.Column([])
+control_col = sg.Column([
+    [sg.Checkbox('Flip x', key = '-FLIPX-'), sg.Checkbox('Flip y', key = '-FLIPY-')],
+    [sg.Button('Save image', key = '-SAVE-')]
+])
 image_col = sg.Column([[sg.Image('test.png')]])
 
-layout = [[image_col]]
+layout = [[control_col, image_col]]
 
 window = sg.Window('Image Editor', layout)
 
