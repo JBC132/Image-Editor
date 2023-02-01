97058195c2efc441538ff99a5a99fe71fd19b04a
이진영 <jakerhee321@gmail.com>
2023-02-01 23:16:18 +0900

    update image2

--- Image Editor.py
+++ Image Editor.py
@@ -3,6 +3,7 @@ import PySimpleGUI as sg
 def update_image(original, blur, contrast, emboss, contour, flipx, flipy):
     pass
 
+image_path = ''
 control_col = sg.Column([
     [sg.Frame('Blur', layout = [[sg.Slider(range = (0,10), orientation = 'h', key = '-BLUR-')]])],
     [sg.Frame('Contrast', layout = [[sg.Slider(range = (0,10), orientation = 'h', key = '-CONTRAST-')]])],
@@ -11,14 +12,24 @@ control_col = sg.Column([
     [sg.Button('Save image', key = '-SAVE-')]
 ])
 image_col = sg.Column([[sg.Image('test.png')]])
-
 layout = [[control_col, image_col]]
 
+original = Image.open(image_path)
 window = sg.Window('Image Editor', layout)
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+    
+    update_image(
+        original,
+        values['-BLUR-'],
+        values['-CONTRAST-'],
+        values['-EMBOSS-'],
+        values['-CONTOUR-'],
+        values['-FLIPX-'],
+        values['-FLIPY-'],
+    )
 
 window.close()
