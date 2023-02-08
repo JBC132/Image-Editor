a2a71401b5aa060f9ec79fd65cd7bae6c435722b
이진영 <jakerhee321@gmail.com>
2023-02-08 20:01:26 +0900

    BytesIO

--- Image Editor.py
+++ Image Editor.py
@@ -1,10 +1,17 @@
 import PySimpleGUI as sg
 from PIL import Image, ImageFilter
+from io import BytesIO 
 
 def update_image(original, blur, contrast, emboss, contour, flipx, flipy):
     image = original.filter(ImageFilter.GaussianBlur(blur))
+    
+    bio = BytesIO()
+    image.save(bio, format = 'PNG')
 
-image_path = 'test.png'
+    window['-IMAGE-'].update(data = bio.getvalue())
+
+image_path = 'Test.png'
+
 control_col = sg.Column([
     [sg.Frame('Blur', layout = [[sg.Slider(range = (0,10), orientation = 'h', key = '-BLUR-')]])],
     [sg.Frame('Contrast', layout = [[sg.Slider(range = (0,10), orientation = 'h', key = '-CONTRAST-')]])],
@@ -12,7 +19,7 @@ control_col = sg.Column([
     [sg.Checkbox('Flip x', key = '-FLIPX-'), sg.Checkbox('Flip y', key = '-FLIPY-')],
     [sg.Button('Save image', key = '-SAVE-')]
 ])
-image_col = sg.Column([[sg.Image(image_path)]])
+image_col = sg.Column([[sg.Image(image_path, key = '-IMAGE-')]])
 layout = [[control_col, image_col]]
 
 original = Image.open(image_path)
